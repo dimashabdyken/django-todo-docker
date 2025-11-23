7b30bb76c8ca06032407adec67b4ce0ab641c5b9
dimashabdyken <abdykendinmuhammed@gmail.com>
2025-11-23 20:42:39 +0500

    Fix: restart.sh modified

--- restart.sh
+++ restart.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "Убиваем старые контейнеры"
-# Удаляем контейнеры по их жестким именам, игнорируя ошибки, если их нет
 docker rm -f django_web nginx_proxy postgres_db || true
 
 echo "Чистка сирот"
