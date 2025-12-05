f4b74243e6fa266c74f6a0f4f88d5481bfecab86
dimashabdyken <abdykendinmuhammed@gmail.com>
2025-12-05 17:19:25 +0500

    New feature: terraform was added for deploy

---
+++ terraform/main.tf
@@ -0,0 +1,131 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+# стройка сети (VPC) 
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+  
+  tags = {
+    Name = "My-Terraform-VPC"
+  }
+}
+
+# Шлюз в интернет (чтобы сервер видел мир)
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+# Маршруты 
+resource "aws_route_table" "prod_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+# Подсеть 
+resource "aws_subnet" "my_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true # ВАЖНО: Дать серверу публичный IP
+  availability_zone       = "eu-central-1a"
+}
+
+# Привязываем подсеть к маршрутам
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.prod_route_table.id
+}
+
+
+# образ Ubuntu
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+# Настраиваем Firewall (в новой сети my_vpc)
+resource "aws_security_group" "web_sg" {
+  name        = "jenkins_demo_sg"
+  description = "Allow SSH, HTTP and Jenkins"
+  vpc_id      = aws_vpc.my_vpc.id # <-- ССЫЛАЕМСЯ НА НАШУ СЕТЬ
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    description = "Jenkins"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+#  Ключи 
+resource "aws_key_pair" "deployer" {
+  key_name   = "my-laptop-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+# Сервер (в новой подсети)
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  
+  subnet_id     = aws_subnet.my_subnet.id # <-- СТАВИМ В НАШУ ПОДСЕТЬ
+  key_name      = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  tags = {
+    Name = "Django-Server-From-Terraform"
+  }
+}
+
+output "instance_ip" {
+  value = aws_instance.app_server.public_ip
+}
